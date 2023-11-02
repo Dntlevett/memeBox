8bb3a5c93edf7fc126eb65193e8369641250ac75
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-02 12:21:10 -0700

    add function for retrieving meme and create use effect and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  const StylishButton = () => {
-    return <button className="main-form__button">Click Me</button>;
-  };
-  StylishButton();
+  // const StylishButton = () => {
+  //   return <button className="main-form__button">Click Me</button>;
+  // };
+  // StylishButton();
+
+  function handlePushButton(event) {
+    event.preventDefault();
+    // need to adjust this link to source files
+    // const response = axios.get(`${process.env.REACT_APP_API_URL}/public`);
+    alert("video test");
+  }
+
+  const [memes, setMemes] = useState([]);
+  const [selectedMeme, setSelectedMeme] = useState({});
+
+  useEffect(() => {
+    // const response = axios.get`${process.env.REACT_APP_API_URL}/data/memes.json`;
+    const response = axios.get(`http://localhost:5050`);
+    // console.log(response);
+    setMemes(response.data);
+  }, []);
+
   return (
     <div className="main-container">
       <h1>MeMeBoX</h1>
-      <form action="" className="main-form">
-        <img src="" alt="" className="main-form__image" />
+      <form
+        onSubmit={(event) => {
+          handlePushButton(event);
+        }}
+        className="main-form"
+      >
+        {/* {memes
+          .filter((meme) => meme.id !== selectedMeme.id)
+          .map((meme) => {
+            return (
+              <img
+                src={`${process.env.REACT_APP_API_URL}/data/${meme.url}`}
+                alt=""
+                className="main-form__image"
+              />
+            );
+          })} */}
+
         <button className="main-form__button"></button>
       </form>
     </div>
